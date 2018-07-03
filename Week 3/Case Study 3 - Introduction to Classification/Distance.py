039b8be65d347c70637b0734dbf8e39dbb2d0a82
roshan2M <r2munjal@edu.uwaterloo.ca>
2018-07-02 21:49:12 -0400

    Add functions to compute the distance between points and find majority votes in a list.

---
+++ Week 3/Case Study 3 - Introduction to Classification/Distance.py
@@ -0,0 +1,35 @@
+import numpy as np
+import random
+import scipy.stats as ss
+
+def distance(p1: np.array, p2: np.array) -> float:
+    """
+    Finds the Euclidean distance between points p1 and p2, represented as numpy
+    arrays.
+    """
+    return np.sqrt(np.sum(np.power(p2 - p1, 2)))
+
+def majority_vote(votes: list):
+    """
+    Finds the key which has the most votes in list votes. Returns a random element
+    from the list of winners found in the case of ties.
+    """
+    vote_counts = {}
+    for vote in votes:
+        if vote in vote_counts:
+            vote_counts[vote] += 1
+        else:
+            vote_counts[vote] = 1
+    winners = []
+    max_count = max(vote_counts.values())
+    for vote, count in vote_counts:
+        if count == max_count:
+            winners.append(vote)
+    return random.choice(winners)
+
+def majority_vote_fast(votes: list):
+    """
+    Returns the most common element in votes. In case of ties, picks the first winner.
+    """
+    mode, count = ss.mstats.mode(votes)
+    return mode
