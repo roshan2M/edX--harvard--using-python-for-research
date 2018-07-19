9f6a38a8d5991cd4e4c38e03abc457467c73c762
roshan2M <r2munjal@edu.uwaterloo.ca>
2018-07-19 07:56:16 -0400

    Add correlation matrices of whiskies.

--- Week 4/Case Study 4 - Classifying Whiskies/Whiskies.py
+++ Week 4/Case Study 4 - Classifying Whiskies/Whiskies.py
@@ -1,7 +1,23 @@
 import numpy as np
 import pandas as pd
+import matplotlib.pyplot as plt
 
-whiskies = pd.read_csv("whiskies.txt")
-whiskies["Region"] = pd.read_csv("regions.txt")
+def load_whiskies() -> pd.DataFrame:
+    whiskies = pd.read_csv("whiskies.txt")
+    whiskies["Region"] = pd.read_csv("regions.txt")
+    return whiskies
 
-print(whiskies)
+def load_correlation_matrix(corr_flavours: pd.DataFrame, file_name: str) -> None:
+    plt.figure(figsize=(10,10))
+    plt.pcolor(corr_flavours)
+    plt.axis("tight")
+    plt.colorbar()
+    plt.savefig(file_name)
+
+whiskies = load_whiskies()
+flavours = whiskies.iloc[:, 2:14]
+corr_flavours = pd.DataFrame.corr(flavours)
+load_correlation_matrix(corr_flavours, "Graph to Show Pearson's Coefficient Between Flavours of Whiskies.pdf")
+
+corr_whisky = pd.DataFrame.corr(flavours.transpose())
+load_correlation_matrix(corr_whisky, "Graph to Show Pearson's Coefficient Between Distilleries of Whiskies.pdf")
