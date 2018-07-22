586dc747982a0d98c43846bd3c4e4712e4ae4b07
roshan2M <r2munjal@edu.uwaterloo.ca>
2018-07-21 23:07:54 -0400

    Use Cartopy library to visualize bird trajectories on a map.

--- Week 4/Case Study 5 - Bird Migration/BirdTrajectory.py
+++ Week 4/Case Study 5 - Bird Migration/BirdTrajectory.py
@@ -1,10 +1,12 @@
 import matplotlib.pyplot as plt
+import cartopy.crs as ccrs
+import cartopy.feature as cfeature
 import pandas as pd
 import numpy as np
 
 DEGREE_SYMBOL = u'\N{DEGREE SIGN}'
 
-def plot_all_migrations(bird_data: pd.DataFrame):
+def plot_all_migrations(bird_data: pd.DataFrame) -> None:
     bird_names = pd.unique(bird_data.bird_name)
     plt.figure(figsize=(7,7))
     for name in bird_names:
@@ -17,5 +19,24 @@ def plot_all_migrations(bird_data: pd.DataFrame):
     plt.legend(loc="lower right")
     plt.savefig("Migration of Plot of All Birds.pdf")
 
+def plot_cartopy_migrations(bird_data: pd.DataFrame) -> None:
+    bird_names = pd.unique(bird_data.bird_name)
+    proj = ccrs.Mercator()
+    plt.figure(figsize=(10,10))
+    ax = plt.axes(projection=proj)
+    ax.set_extent((-25.0, 20.0, 52.0, 10.0))
+    ax.add_feature(cfeature.LAND)
+    ax.add_feature(cfeature.OCEAN)
+    ax.add_feature(cfeature.COASTLINE)
+    ax.add_feature(cfeature.BORDERS, linestyle=':')
+    for name in bird_names:
+        index = bird_data.bird_name == name
+        x, y = bird_data.longitude[index], bird_data.latitude[index]
+        ax.plot(x, y, ".", transform=ccrs.Geodetic(), label=name)
+    plt.title("Plot to Show Migration of Birds Over Half Hour Intervals")
+    plt.legend(loc="upper left")
+    plt.savefig("Bird Migration Map.pdf")
+
 bird_data = pd.read_csv("bird_tracking.csv")
 plot_all_migrations(bird_data)
+plot_cartopy_migrations(bird_data)
